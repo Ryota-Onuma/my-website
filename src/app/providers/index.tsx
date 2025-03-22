20ece1249f6698c3f8a95b452cf345798cdc3ddd
Ryota-Onuma <onumaryotajh.tech@gmail.com>
2025-03-22 23:54:01 +0900

    rm <Theme>

--- src/app/providers/index.tsx
+++ src/app/providers/index.tsx
@@ -1,8 +1,6 @@
 import { UIProvider } from "./ui";
 import { LoadingProvider } from "@/app/providers/loading";
 import { ErrorProvider } from "@/app/providers/error";
-import { Theme } from "@chakra-ui/react";
-import { useColorMode } from "@/app/components/ui/theme";
 import ErrorBoundaryProvider from "@/app/providers/error-boundary";
 
 type AppProviderProps = {
@@ -14,16 +12,9 @@ export const AppProvider = ({ children }: AppProviderProps) => {
     <UIProvider>
       <ErrorBoundaryProvider>
         <ErrorProvider>
-          <LoadingProvider>
-            <ThemeWrapper>{children}</ThemeWrapper>
-          </LoadingProvider>
+          <LoadingProvider>{children}</LoadingProvider>
         </ErrorProvider>
       </ErrorBoundaryProvider>
     </UIProvider>
   );
-};
-
-const ThemeWrapper = ({ children }: { children: React.ReactNode }) => {
-  const { colorMode } = useColorMode(); // ✅ 必ずUIProvider（next-themes）の内側で呼ぶ
-  return <Theme appearance={colorMode}>{children}</Theme>;
 };
